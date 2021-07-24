49500ff5c2498114c5c25002e6c6d5735dd6a59c
MacaronFR <denisft77@gmail.com>
2021-07-24 12:27:09 +0200

    remake controller.php

---
+++ root.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var string $head The content of the head of the page
+ * @var string $body The content of the body of the page
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <?= $head ?>
+</head>
+<body>
+    <?= $body ?>
+</body>
+</html>
