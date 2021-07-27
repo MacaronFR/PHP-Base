36b543a583d5caee9d291655055460b55ffc8544
MacaronFR <denisft77@gmail.com>
2021-07-27 11:08:22 +0200

    change on Controller.php

--- Controller.php
+++ Controller.php
@@ -49,48 +49,52 @@ abstract class Controller{
 	 */
 	#[NoReturn]public abstract function delete();
 
-    /**
-     * @param string $head String to add to the default head
-     * @param string $header String to add to the default header
-     * @param string $main String to add to the default main
-     * @param string $footer String to add to the default footer
-     */
+	/**
+	 * @param string $head String to add to the default head
+	 * @param string $header String to add to the default header
+	 * @param string $main String to add to the default main
+	 * @param string $footer String to add to the default footer
+	 */
 	protected function generatePage(string $head = "", string $header = "", string $main = "", string $footer = ""){
-	    $head = $this->generateHead($head);
-	    $body = $this->generateBody($header, $main, $footer);
-	    include "root.php";
-    }
+		$head = $this->generateHead($head);
+		$body = $this->generateBody($header, $main, $footer);
+		include "root.php";
+	}
 
-    /**
-     * @param string $head line to add to the default head of the page
-     * @return string
-     */
-    protected function generateHead(string $head = ""): string{
-	    ob_start();
-	    //make all work to have a proper head
-        return ob_get_clean() . $head;
-    }
+	/**
+	 * @param string $head line to add to the default head of the page
+	 * @return string
+	 */
+	protected function generateHead(string $head = ""): string{
+		ob_start();
+		//make all work to have a proper head
+		return ob_get_clean() . $head;
+	}
 
-    protected function generateBody(string $header = "", string $main = "", string $footer = ""): string{
-        $header = $this->generateHeader() . $header;
-        $main = $this->generateMain() . $main;
-        $footer = $this->generateFooter() . $footer;
-        ob_start();
-        include "body.php";
-        return ob_get_clean();
-    }
+	protected function generateBody(string $header = "", string $main = "", string $footer = ""): string{
+		$header = $this->generateHeader() . $header;
+		$main = $this->generateMain() . $main;
+		$footer = $this->generateFooter() . $footer;
+		ob_start();
+		include "body.php";
+		return ob_get_clean();
+	}
 
-    protected function generateHeader(): string{
-        ob_start();
-        //make all work to have a proper page header
-        return ob_get_clean();
-    }
+	protected function generateHeader(): string{
+		ob_start();
+		//make all work to have a proper page header
+		return ob_get_clean();
+	}
 
-    protected abstract function generateMain(): string;
+	protected function generateMain(): string{
+		ob_start();
+		//make all work to have a proper page content
+		return ob_get_clean();
+	}
 
 	protected function generateFooter(): string{
-	    ob_start();
-	    //make all work to get a proper page footer
-        return ob_get_clean();
-    }
+		ob_start();
+		//make all work to get a proper page footer
+		return ob_get_clean();
+	}
 }
